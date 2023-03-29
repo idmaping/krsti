d927c846bac9bea9842d67d68717731b6127f7d6
Firman Ardiyansyah <firman.ardiyansyah@formulatrix.com>
2023-03-29 13:31:19 +0700

    remove unused code

--- csharp/Aerora/Aerora/Program.cs
+++ csharp/Aerora/Aerora/Program.cs
@@ -10,18 +10,7 @@ namespace Aerora
     
     static void Main(string[] args)
     {
-      XLService xLService= new XLService();
-      XMService xMService = new XMService();
-
-      List<DXLServiceBase> services = new List<DXLServiceBase>();
-      services.Add(xLService);
-      services.Add(xMService);
-
-      foreach(DXLServiceBase service in services)
-      {
-        service.Execute();
-      }
-
+     
     }
   }
 }
